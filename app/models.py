e81622c54a05c78dfc4eac00df58468f4887139e
Tuan Nguyen Van Anh <tuanio@pop-os.localdomain>
2021-03-04 18:30:52 +0700

    Done database

--- app/models.py
+++ app/models.py
@@ -3,62 +3,120 @@ from app import db
 # - one
 # = many
 
-#[] User   - -  Class 
-#[] Class  - =  Student
-# Student  = =  Subject
+# User   - -  Class
+# Class  - =  Student
 # Semester - =  Subject
+# Student  = =  Subject
+
+# back_populates trỏ đến các tham số của các các class khác
+# db.relation(tham số đầu), tham số đầu tiên là tên của table 
+
+class SubjectDetail(db.Model):
+    __tablename__ = 'SubjectDetail'
+    student_id = db.Column(db.Integer, db.ForeignKey('Student.id'), primary_key=True)
+    subject_id = db.Column(db.Integer, db.ForeignKey('Subject.id'), primary_key=True)
+    score_10 = db.Column(db.Float, nullable=True)
+
+    subjects = db.relation('Subject', back_populates='students')
+    students = db.relation('Student', back_populates='subjects')
+
+    def __init__(self, student_id, subject_id, score_10=None):
+        self.student_id = student_id
+        self.subject_id = subject_id
+        self.score_10 = score_10
+    
+    def __repr__(self):
+        return '<SubjectDetail({}, {}, {})>'.format(self.student_id, self.subject_id, self.score_10)
 
 class User(db.Model):
+    __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), unique=True, nullable=False)
     full_name = db.Column(db.String(200), nullable=False)
-    class_ = db.relationship('Class_', backref='User', uselist=False)
 
-    def __init__(self, username, password_hash, full_name, class_=None):
+    # one class
+    # uselist=False to set relationship to one one
+    class_ = db.relationship('Class_', backref='User', lazy=True, uselist=False)
+
+    def __init__(self, username, password_hash, full_name):
         self.username = username
         self.password_hash = password_hash
         self.full_name = full_name
-        self.class_ = class_
 
     def __repr__(self):
-        return "<User({}, {}, {}, {})>".format(self.id, self.username, self.full_name, self.class_)
+        return "<User({}, {}, {})>".format(self.id, self.username, self.full_name)
 
 class Class_(db.Model):
+    __tablename__ = 'Class_'
     id = db.Column(db.Integer, primary_key=True)
     class_name = db.Column(db.String(50), nullable=False, unique=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(User.id), unique=True)
-    students = db.relationship('Student', backref='class_')
 
-    def __init__(self, class_name, user_id = None, students = None):
+    # one user
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+
+    # many student
+    students = db.relationship('Student', backref='Class_', lazy=True, uselist=True)
+
+    def __init__(self, class_name, user_id):
         self.class_name = class_name
         self.user_id = user_id
     
     def __repr__(self):
-        return "<Class_({}, {})>".format(self.class_name, self.user_id)
+        return "<Class_({}, {})>".format(self.id, self.class_name)
 
 class Student(db.Model):
+    __tablename__ = 'Student'
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(200), nullable=False)
     student_code = db.Column(db.String(10), nullable=False, unique=True)
     course = db.Column(db.Integer, nullable=False)
-    class_id = db.Column(db.Integer, db.ForeignKey(Class_.id))
 
-    def __init__(self, full_name, student_code, course, class_id = None):
+    # one class
+    class_id = db.Column(db.Integer, db.ForeignKey('Class_.id'), nullable=False)
+
+    # many to many
+    subjects = db.relationship(SubjectDetail, back_populates='students')
+
+    def __init__(self, full_name, student_code, course, class_id):
         self.full_name = full_name
         self.student_code = student_code
         self.course = course
         self.class_id = class_id
 
     def __repr__(self):
-        return "<Student({}, {}, {}, {})>".format(full_name, student_code, course, class_id)
+        return "<Student({}, {}, {}, {})>".format(self.id, self.full_name, self.student_code, self.course)
 
 class Subject(db.Model):
+    __tablename__ = 'Subject'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False, unique=True)
     credit = db.Column(db.Integer, default=0, nullable=False)
 
+    # one semester
+    semester_id = db.Column(db.Integer, db.ForeignKey('Semester.id'), nullable=False)
+
+    # many to many
+    students = db.relationship(SubjectDetail, back_populates='subjects')
+
+    def __init__(self, name, credit, semester_id):
+        self.name = name
+        self.credit = credit
+        self.semester_id = semester_id
+
+    def __repr__(self):
+        return '<Subject({}, {}, {})>'.format(self.id, self.name, self.credit)
+
 class Semester(db.Model):
+    __tablename__ = 'Semester'
     id = db.Column(db.Integer, primary_key=True)
     number = db.Column(db.Integer, nullable=False, default=1)
     
+    # many subjects
+    subjects = db.relationship(Subject, backref='Semester', uselist=True)
+
+    def __init__(self, number):
+        self.number = number
+
+    def __repr__(self):
+        return '<Semester({}, {})>'.format(self.id, self.number)
--- init_db.py
+++ init_db.py
@@ -1,22 +1,67 @@
 from app import *
 from app.models import *
+
 db.drop_all()
-db.create_all()
+# drop database by order
+# Subject.__table__.drop(db.engine)
+# Semester.__table__.drop(db.engine)
+# Student.__table__.drop(db.engine)
+# SubjectDetail.__table__.drop(db.engine)
+# Class_.__table__.drop(db.engine)
+# User.__table__.drop(db.engine)
 
-a_class = Class_('DHKHDL15A')
+db.create_all() 
 
-a_user = User('trungbe', 'trumbquanxip', 'Phạm Thành Bưởi', class_=a_class)
+a_user = User(username='tuanio', password_hash='asdfasdfasfsaddf', full_name='Nguyễn Văn Anh Tuấn')
+
+
+db.session.add(a_user)
+db.session.commit()
+
+print(a_user)
+
+a_class = Class_('Đại Học Khoa Học Dữ Liệu 15A', user_id=a_user.id)
 
 db.session.add(a_class)
-db.session.add(a_user)
+db.session.commit()
 
-fulls = ['toan', 'tuan', 'trung']
-code = ['12', '13', '14']
-course = [15, 15, 15]
+student_fullname = ['Phạm Thành Trung', 'Huỳnh Minh Toàn', 'Nguyễn Bô Lão']
+student_course = [15, 16, 15]
+student_code = ['250880502', '66770289', '12345678']
 
-a_class = Class_.query.all()[-1]
+students = []
+for i in range(3):
+    foo = Student(full_name=student_fullname[i], student_code=student_code[i], course=student_course[i], class_id=a_class.id)
+    students += [foo]
 
-for i in range(len(code)):
-    foo = Student(full_name=fulls[i], student_code=code[i], course=course[i], class_id=a_class.id)
-    db.session.add(foo)
+for i in students:
+    db.session.add(i)
+
+semester1 = Semester(1)
+semester2 = Semester(2)
+semester3 = Semester(3)
+
+db.session.add(semester1)
+db.session.add(semester2)
+db.session.add(semester3)
+db.session.commit()
+
+subjects = []
+subjects += [Subject('Toán cao cấp 1', 3, semester_id=semester1.id)]
+subjects += [Subject('Thống kê máy tính và ứng dụng', 3, semester_id=semester3.id)]
+subjects += [Subject('Hệ thống máy tính', 3, semester_id=semester2.id)]
+
+for i in subjects:
+    db.session.add(i)
+db.session.commit()
+
+import random
+
+data = []
+for student in students:
+    for subject in subjects:
+        data += [SubjectDetail(student_id=student.id, subject_id=subject.id, score_10=random.randint(0, 10))]
+
+for i in data:
+    db.session.add(i)
 db.session.commit()
